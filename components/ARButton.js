cb0ed234f8e10c7b1ad6c1dd27b2eb4ee5c68f23
Shahzaib Sarwar <imshahzayb@gmail.com>
2020-12-21 16:10:33 +0500

    fix

--- components/ARButton.js
+++ components/ARButton.js
@@ -2,18 +2,7 @@ import React from "react";
 import "@leoncvlt/ar-button";
 
 function ARButton() {
-  return (
-    <div>
-      <ar-button
-        src="https://model-viewer-demo.vercel.app/models/666.glb"
-        ios-src="https://model-viewer-demo.vercel.app/models/666.usdz"
-        link="https://model-viewer-demo.vercel.app/"
-        title="Model 1"
-      >
-        See in Augmented Reality
-      </ar-button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default ARButton;
--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,14 @@ export default function Home() {
         </div> */}
         <div style={{ padding: "2rem" }}>
           <DynamicARButton />
+          <ar-button
+            src="https://model-viewer-demo.vercel.app/models/666.glb"
+            ios-src="https://model-viewer-demo.vercel.app/models/666.usdz"
+            link="https://model-viewer-demo.vercel.app/"
+            title="Model 1"
+          >
+            See in Augmented Reality
+          </ar-button>
         </div>
       </main>
     </div>
